eee4862667d175f2bd2881e54dddd6beeb122321
pashamartinenko <martipash@gmail.com>
2023-04-21 21:27:58 +0400

    given-when-then

--- spring-core/spring-core/src/test/java/org/jmp/spring/core/dao/EventDaoTest.java
+++ spring-core/spring-core/src/test/java/org/jmp/spring/core/dao/EventDaoTest.java
@@ -14,56 +14,64 @@ import java.util.Map;
 class EventDaoTest
 {
 
-    private static EventDaoImpl eventDao = new EventDaoImpl();
+    private static final EventDaoImpl eventDao = new EventDaoImpl();
 
     @Test
     void getEventById_IdExists_EventIsReturned() {
+        // GIVEN
         EventImpl expectedEvent = new EventImpl(321L, "Title", new Date());
         Storage storage = new Storage();
         storage.setStorageMap(Map.of("event:321", expectedEvent));
         eventDao.setStorage(storage);
 
+        // WHEN
         Event event = eventDao.getEventById(expectedEvent.getId());
 
+        // THEN
         Assertions.assertEquals(expectedEvent, event);
     }
 
     @Test
     void getEventById_IdDoesNotExist_NullIsReturned() {
+        // GIVEN
         long nonExistentId = 321L;
         Storage storage = new Storage();
         storage.setStorageMap(Map.of());
         eventDao.setStorage(storage);
-        EventImpl expectedEvent = new EventImpl(211L, "Title 2", new Date());
 
+        // WHEN
         Event event = eventDao.getEventById(nonExistentId);
 
+        // THEN
         Assertions.assertNull(event);
     }
 
     @Test
     void getEventsByTitle_ContainsTitle_ListOfEventsAreReturned() {
+        // GIVEN
         EventImpl expectedEvent = new EventImpl(321L, "2123 Title", new Date());
         EventImpl anExpectedEvent = new EventImpl(232L, "Title 232", new Date());
         EventImpl wrongEvent = new EventImpl(21112L, "Film 232", new Date());
         List<Event> expectedEvents = List.of(expectedEvent, anExpectedEvent);
-
-
         Storage storage = new Storage();
         storage.setStorageMap(Map.of("event:321", expectedEvent,
                                      "event:232", anExpectedEvent,
                                      "event:211", wrongEvent));
         eventDao.setStorage(storage);
 
+        // WHEN
         List<Event> events = eventDao.getEventsByTitle("Title", 3, 1);
+
+        // THEN
         Assertions.assertAll(() -> {
-            Assertions.assertTrue(expectedEvents.size() == events.size());
+            Assertions.assertEquals(expectedEvents.size(), events.size());
             Assertions.assertTrue(expectedEvents.containsAll(events));
         });
     }
 
     @Test
     void getEventsByTitle_TitleDoesNotExist_EmptyList() {
+        // GIVEN
         Storage storage = new Storage();
         storage.setStorageMap(Map.of(
                 "event:321", new EventImpl(321L, "2123 Title", new Date()),
@@ -71,8 +79,10 @@ class EventDaoTest
                     "event:21112", new EventImpl(21112L, "Film 232", new Date())));
         eventDao.setStorage(storage);
 
+        // WHEN
         List<Event> events = eventDao.getEventsByTitle("Non existing title", 2, 1);
 
+        // THEN
         Assertions.assertEquals(List.of(), events);
     }
 }

28b3941f3d329cfcc5b0a33fcee2165cedaf6281
pashamartinenko <martipash@gmail.com>
2023-08-12 13:51:08 +0400

    eventController getEventsForDay getEventsByTitle

--- spring-core/spring-core/src/main/java/org/jmp/spring/core/controller/EventController.java
+++ spring-core/spring-core/src/main/java/org/jmp/spring/core/controller/EventController.java
@@ -6,24 +6,49 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jmp.spring.core.facade.BookingFacade;
 import org.jmp.spring.core.model.impl.EventImpl;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/v1")
 public class EventController
 {
+    private static final String EVENTS_VIEW = "events";
     private final BookingFacade bookingFacade;
 
-    @GetMapping("/events/{eventId}")
+    @GetMapping("/events/id/{eventId}")
     public ModelAndView getEventById(@PathVariable Long eventId) {
-        log.info(format("GET /events/%d", eventId));
-        ModelAndView modelAndView = new ModelAndView("event");
+        log.info(format("GET events by id=%d", eventId));
+        ModelAndView modelAndView = new ModelAndView(EVENTS_VIEW);
         EventImpl event = bookingFacade.getEventById(eventId);
-        modelAndView.addObject("event", event);
+        modelAndView.addObject("events", List.of(event));
+        return modelAndView;
+    }
+
+    @GetMapping("/events/title/{title}")
+    public ModelAndView getEventsByTitle(@PathVariable String title, @RequestParam(defaultValue = "100") Integer pageSize, @RequestParam(name = "offset", defaultValue = "1") Integer pageNum) {
+        log.info(format("GET events by title=%s, pageSize=%d, pageNum=%d", title, pageSize, pageNum));
+        ModelAndView modelAndView = new ModelAndView(EVENTS_VIEW);
+        List<EventImpl> events = bookingFacade.getEventsByTitle(title, pageSize, pageNum);
+        modelAndView.addObject("events", events);
+        return modelAndView;
+    }
+
+    @GetMapping("/events/day/{day}")
+    public ModelAndView getEventsForDay(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date day, @RequestParam(defaultValue = "100") Integer pageSize, @RequestParam(name = "offset", defaultValue = "1") Integer pageNum) {
+        log.info(format("GET events by day=%s, pageSize=%d, pageNum=%d", day, pageSize, pageNum));
+        ModelAndView modelAndView = new ModelAndView(EVENTS_VIEW);
+        List<EventImpl> events = bookingFacade.getEventsForDay(day, pageSize, pageNum);
+        modelAndView.addObject("events", events);
         return modelAndView;
     }
 }

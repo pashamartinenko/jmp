2a0778576bae7d1529dfc025c9dfc114aea8fc78
pmartynenkaEPAM <pmartynenka@idbs.com>
2023-09-09 18:29:49 +0400

    merge master

---
+++ spring-mvc/src/main/java/org/jmp/spring/core/dao/TicketDao.java
@@ -0,0 +1,18 @@
+package org.jmp.spring.core.dao;
+
+import org.jmp.spring.core.model.impl.TicketImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TicketDao extends CrudRepository<TicketImpl, Long>
+{
+    @Query("SELECT t from TicketImpl t WHERE t.user.id = :userId")
+    List<TicketImpl> findByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT t from TicketImpl t WHERE t.event.id = :eventId")
+    List<TicketImpl> findByEventId(@Param("eventId") Long eventId, Pageable pageable);
+}
---
+++ spring-mvc/src/main/java/org/jmp/spring/core/dao/UserDao.java
@@ -0,0 +1,16 @@
+package org.jmp.spring.core.dao;
+
+import org.jmp.spring.core.model.impl.UserImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserDao extends CrudRepository<UserImpl, Long>
+{
+    UserImpl findByEmail(String email);
+
+    List<UserImpl> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    UserImpl findByIdAndNameAndEmail(Long id, String name, String email);
+}
